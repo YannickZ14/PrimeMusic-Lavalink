5ba7cd03782c2aa79f026c52992f5bd6af348037
YannickZ14 <info@yannickgroup.org>
2025-02-02 17:17:26 +0100

    Update en.js

--- languages/en.js
+++ languages/en.js
@@ -1,5 +1,5 @@
 module.exports = {
-    footer: "Developed by SSRR | Prime Music v1.2",
+    footer: "Developed by Pegagus Clan | English Musik Bot",
     ping: {
       description: "Check the bot latency",
       response: "Pinging...",
